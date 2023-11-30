588ea8e6347a43945a7e3719987f22837bac25ca
Maxim <334890@gmail.com>
2023-11-30 10:35:34 +0600

    simplify tableButton component

--- src/components/UI/TableButton/TableButton.jsx
+++ src/components/UI/TableButton/TableButton.jsx
@@ -2,10 +2,8 @@ import styles from "./TableButton.module.css";
 
 export default function TableButton({ children, onClick }) {
   return (
-    <div className={styles.tableButtonContainer}>
-      <button className={styles.TableButton} onClick={onClick}>
-        {children}
-      </button>
-    </div>
+    <button className={styles.TableButton} onClick={onClick}>
+      {children}
+    </button>
   );
 }
